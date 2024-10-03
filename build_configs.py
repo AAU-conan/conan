de28569ef51eec0be122e8025753d7f59f015c42
Rasmus Tollund <rgt@cs.aau.dk>
2024-10-03 10:27:13 +0200

    Changed build to disable symbolic

--- build_configs.py
+++ build_configs.py
@@ -1,5 +1,7 @@
-release = ["-DCMAKE_BUILD_TYPE=Release"]
-debug = ["-DCMAKE_BUILD_TYPE=Debug"]
+no_cudd = ['-DUSE_CUDD=FALSE', '-DLIBRARY_SYMBOLIC_ENABLED=FALSE', '-DLIBRARY_SYMBOLIC_SEARCH_ENABLED=FALSE']
+
+release = ["-DCMAKE_BUILD_TYPE=Release"] + no_cudd
+debug = ["-DCMAKE_BUILD_TYPE=Debug"] + no_cudd
 release_no_lp = ["-DCMAKE_BUILD_TYPE=Release", "-DUSE_LP=NO"]
 # USE_GLIBCXX_DEBUG is not compatible with USE_LP (see issue983).
 glibcxx_debug = ["-DCMAKE_BUILD_TYPE=Debug", "-DUSE_LP=NO", "-DUSE_GLIBCXX_DEBUG=YES"]
