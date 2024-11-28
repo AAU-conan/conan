3b0bc4ab5bda59a8faf1549fb1f241608772ff2a
Rasmus Tollund <rgt@cs.aau.dk>
2024-11-28 15:34:10 +0100

    Added comparative heuristic

---
+++ src/search/heuristics/comparative_heuristic.cc
@@ -0,0 +1,64 @@
+#include "comparative_heuristic.h"
+#include "../plugins/plugin.h"
+#include <print>
+#include <ranges>
+#include <format>
+#include <boost/algorithm/string/join.hpp>
+#include <boost/algorithm/string/detail/classification.hpp>
+
+class Evaluator;
+
+EvaluationResult ComparativeHeuristic::compute_result(EvaluationContext& eval_context) {
+    // bool first = true;
+    // EvaluationResult first_result;
+    // for (const auto& heuristic : comparison_heuristics) {
+    //     const auto result = heuristic->compute_result(eval_context);
+    //     if (first) {
+    //         first_result = result;
+    //         first = false;
+    //     }
+    //     std::print("{}: {}, ", heuristic->get_description(), result.get_evaluator_value());
+    // }
+    // Use ranges instead
+    const auto results = comparison_heuristics | std::views::transform([&eval_context](const auto& heuristic) {
+        return heuristic->compute_result(eval_context);
+    });
+    std::println("{}", boost::algorithm::join(std::views::zip(comparison_heuristics, results) | std::views::transform([](const std::pair<std::shared_ptr<Evaluator>, EvaluationResult>& pair) {
+        return std::format("{}: {}", pair.first->get_description(), pair.second.get_evaluator_value());
+    }) | std::ranges::to<std::vector<std::string>>(), ", "));
+    return results.front();
+}
+
+int ComparativeHeuristic::compute_heuristic(const State& ancestor_state) {
+    throw std::runtime_error("Should not be reachable");
+}
+
+
+class ComparativeHeuristicFeature
+    : public plugins::TypedFeature<Evaluator, ComparativeHeuristic> {
+public:
+    ComparativeHeuristicFeature() : TypedFeature("comparative") {
+        document_title("Compare Multiple Heuristics");
+        document_synopsis("Compare multiple heuristics by running search with the first heuristic, but evaluating all heuristics for each state.");
+
+        add_list_option<std::shared_ptr<Evaluator>>(
+            "heuristics",
+            "Heuristics to compare, the first in the list will be used for search");
+
+        add_heuristic_options_to_feature(*this, "comparative");
+    }
+
+    virtual std::shared_ptr<ComparativeHeuristic> create_component(
+        const plugins::Options &opts,
+        const utils::Context &context) const override {
+        plugins::verify_list_non_empty<std::shared_ptr<Evaluator>>(
+            context, opts, "heuristics");
+        return plugins::make_shared_from_arg_tuples<ComparativeHeuristic>(
+            opts.get_list<std::shared_ptr<Evaluator>>( "heuristics"),
+            get_heuristic_arguments_from_options(opts)
+        );
+    }
+};
+
+static plugins::FeaturePlugin<ComparativeHeuristicFeature> _plugin;
+
